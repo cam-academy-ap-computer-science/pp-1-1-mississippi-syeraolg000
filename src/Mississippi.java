e5fa8b18b0d0072034319619182eae23c648ab6e
syeraolg000 <syeraolg000@CAM-LAB-28.bgsd.k12.wa.us>
2019-09-23 07:14:31 -0700

    I'm done with this project

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,7 +3,84 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
 	}
-
 }
+		
+		
